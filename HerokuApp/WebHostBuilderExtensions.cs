c213138af49274dc2547b597a309cee7d00e3eda
BRUNO ALVES SIQUEIRA <alvesSiqueira@outlook.com>
2020-06-21 00:13:00 -0300

    Add project files.

---
+++ HerokuApp/WebHostBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HerokuApp
+{
+    public static class WebHostBuilderExtensions
+    {
+        public static IWebHostBuilder UsePort(this IWebHostBuilder builder)
+        {
+            var port = Environment.GetEnvironmentVariable("PORT");
+            if (string.IsNullOrEmpty(port))
+            {
+                return builder;
+            }
+
+            return builder.UseUrls($"http://+:{port}");
+        }
+    }
+}
